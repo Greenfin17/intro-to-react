5247f203e61b70c0eaf117227684ef18720cf39a
Greenfin17 <jmaple@yahoo.com>
2021-04-17 14:04:15 -0500

    starting to work

--- src/Counter.js
+++ src/Counter.js
@@ -2,15 +2,35 @@ import React, { useState } from 'react';
 
 const Counter = () => { 
   const [counter, setCounter] = useState(0);
-  const [userInput, setUserInput] = useState(0);
+  const [userInput, setUserInput] = useState(1);
   const [counterName, setCounterName] = useState("Counter 1");
   
   return (
-    <>
-    <h2>{counter}</h2>
-    <h2>{userInput}</h2>
-    <h2>{counterName}</h2>
-    </>
+    <div>
+      <input 
+        onChange={(e) => setCounterName(e.target.value)}>
+      </input>
+      <h1>{counterName}</h1>
+      <h2>{counter}</h2>
+
+      <input
+        onChange={(e) => setCounterName(e.target.value)}>
+      </input>
+
+      <input 
+        onChange={(e) => setUserInput(Number(e.target.id))}>
+      </input><br />
+
+      <button 
+        onClick={() => setCounter((prevState) => prevState + userInput)}>
+          Increment by {userInput}
+      </button>
+      {counter <= 0 ? '' : 
+        <button onClick={() => setCounter((prevState) => prevState - 1)}
+          >Decrement by {userInput}
+        </button>}
+      <button onClick={() => setCounter(0)}>Reset</button>
+    </div>
   )
 }
 export default Counter;
